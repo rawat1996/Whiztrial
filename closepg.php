e2dda6d4ae023a8e3a5f72da6bce95e3d6251b30
rawat1996 <rsaurabh98@gmail.com>
2017-09-05 18:17:54 +0530

    Todaycheckin0sep
    this is today work

---
+++ closepg.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Close page Test</title>
+</head>
+
+<body>
+<p>click on image to close the window</p>
+<a href="http://127.0.0.1/whizTrial%20Temp/welcome.php?event=popup" target="_blank"  onClick="javascript:window.close()" id="cpg" name="cpag"><img src="whiz.jpg" width="100px" height="50px" alt="whiztrain" /></a>
+
+</body>
+</html>
